1f530dd4b8cd628bc6fbf6b2006e27c57dec38b6
Magzhan Abdibayev <abdibaevmagzhan@gmail.com>
2018-06-30 00:07:00 +0600

    init repo

---
+++ Plugins/StreetMap/Source/StreetMapRuntime/StreetMapRuntime.Build.cs
@@ -0,0 +1,24 @@
+// Copyright 2017 Mike Fricker. All Rights Reserved.
+
+namespace UnrealBuildTool.Rules
+{
+	public class StreetMapRuntime : ModuleRules
+	{
+        public StreetMapRuntime(ReadOnlyTargetRules Target)
+			: base(Target)
+		{
+		PCHUsage = ModuleRules.PCHUsageMode.UseExplicitOrSharedPCHs;
+			PrivateDependencyModuleNames.AddRange(
+				new string[] {
+                    "Core",
+					"CoreUObject",
+					"Engine",
+					"RHI",
+					"RenderCore",
+					"ShaderCore",
+                    "PropertyEditor"
+                }
+			);
+		}
+	}
+}
